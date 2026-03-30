05b0946583a3c2c2d659ec9bba5c14b5f17596bc
bethelcoder <nduvheni066@gmail.com>
2026-03-30 20:47:09 +0200

    this

--- jest.config.js
+++ jest.config.js
@@ -8,12 +8,12 @@ module.exports = {
     "!src/server.js",
     "!src/app.js"
   ],
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 70,
-      functions: 80,
-      lines: 80
-    }
-  }
+  // coverageThreshold: {
+  //   global: {
+  //     statements: 80,
+  //     branches: 70,
+  //     functions: 80,
+  //     lines: 80
+  //   }
+  // }
 };
