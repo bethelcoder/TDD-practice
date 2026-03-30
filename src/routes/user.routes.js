aa33daa1d6ebb9ea283e54682ffa093bb26ba19d
bethelcoder <nduvheni066@gmail.com>
2026-03-30 20:45:22 +0200

    feat: something

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const { registerUser } = require("../controllers/user.controllers");
 
 router.post("/register", registerUser);
-
+router.get("/register", (req, res) => {
+    res.send("Welcome to the reg page")
+});
 module.exports = router;
