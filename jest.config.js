eaceab6cfa0a4b31088052bd74a9369dc1540fc5
bethelcoder <nduvheni066@gmail.com>
2026-03-30 17:55:15 +0200

    fix: excluded server and app.js from test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/*.test.js"],
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.js"],
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/server.js",
+    "!src/app.js"
+  ],
   coverageThreshold: {
     global: {
       statements: 80,
